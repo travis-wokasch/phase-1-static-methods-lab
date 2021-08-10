4cc48dde7e5830e3193d2f48834aff72d69a0ed3
Travis <tjwokasch@gmail.com>
2021-08-10 15:35:26 -0600

    commit tree 993cd12a2e5aa5c31b874f2937e14c1c5d0acafe

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
 class Formatter {
-  //add static methods here
+  static capitalize(str) {
+    return str.charAt(0).toUpperCase() +str.slice(1)
+  }
+
+  static sanitize(str) {
+    return str.replace(/[^A-Za-z0-9-']+/g)
+ }
+
+  static titleize(str) {
+    const words = str.split(" ");
+
+    for (let i = 0; i < words.length; i++) {
+    words[i] = words[i][0].toUpperCase() + words[i].substr(1);
+    }
+    return words.join(" ")
+  }
+
 }
